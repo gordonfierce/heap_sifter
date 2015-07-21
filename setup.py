074e1bca1d89cad574edec24b0bb10771d838cc9
Gordon Fierce <gordon.fierce@gmail.com>
2015-07-20 20:31:53 -0400

    Add a setuptools setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='heap_sifter',
+    version='0.1',
+    py_modules=['heap_sifter'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        heap_sifter=heap_sifter:cli
+    ''',
+)
