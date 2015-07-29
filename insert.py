d93b15d8bb966b8ef9329669443a8411b7bdf0c2
Gordon Fierce <gordon.fierce@gmail.com>
2015-07-29 15:09:45 -0400

    Use a Click prompt.

--- insert.py
+++ insert.py
@@ -11,14 +11,14 @@ def review(todos):
     print(item.text)
     review = input()
     item.text = review
-    
+
 def write_todos(todo_list, file_name):
-    with open(file_name, mode='w') as my_file:
+    with open(file_name, mode='w') as file:
         for item in todo_list:
-            print(item.text, my_file=my_file)
-            
+            print(item.text, file=file)
+
 def read_todos(todo_file):
-    try: 
+    try:
         with open(todo_file) as file:
             todos = [TODO(todo.strip()) for todo in  file if todo != '\n']
         return todos
@@ -30,6 +30,7 @@ def prioritize_or_equal(item_a, item_b):
     print("a: {}".format(item_a))
     print("b: {}".format(item_b))
     choice = input("More important? a/b/(e)qual: ")
+    choice = click.prompt("More important? a/b/(e)qual: ")
     if choice == 'b':
         return 'less'
     elif choice == 'a':
@@ -41,19 +42,19 @@ def prioritize_or_equal(item_a, item_b):
 class TODO:
     def __init__(self, text):
         self.text = text
-    
+
     def __repr__(self):
         return "TODO({})".format(self.text)
-    
+
     def __str__(self):
         return self.text
-    
+
     def __eq__(self, other):
         if self.text < other.text:
             return prioritize_or_equal(self.text, other.text) == 'equal'
         else:
             return prioritize_or_equal(other.text, self.text) == 'equal'
-    
+
     def __lt__(self, other):
         if self.text < other.text:
             return prioritize_or_equal(self.text, other.text) == 'greater'
