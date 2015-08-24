3b227ab67ff72ca3588e68c36a39b835c568d720
Gordon Fierce <gordon.fierce@gmail.com>
2015-08-23 20:45:28 -0400

    Test a basic addition.

--- test_heapsifter.py
+++ test_heapsifter.py
@@ -9,3 +9,8 @@ def test_empty_pop():
         assert result.exit_code == 0
         assert result.output == "No todos!\n"
 
+def test_add_todo():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(heapsifter.add, input="Write a test.")
+        assert result.exit_code == 0
