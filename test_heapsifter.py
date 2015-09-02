754d84e5f2e9a628dc90c795b0015dd74d0b7979
Gordon Fierce <gordon.fierce@gmail.com>
2015-09-01 21:18:56 -0400

    Test adding todos as same-line option.

--- test_heapsifter.py
+++ test_heapsifter.py
@@ -14,3 +14,11 @@ def test_add_todo():
     with runner.isolated_filesystem():
         result = runner.invoke(heapsifter.add, input="Write a test.")
         assert result.exit_code == 0
+
+def test_add_arg():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(heapsifter.add, ["-i", "Pass todo directly."])
+        assert result.exit_code == 0
+        with open('todo.txt') as todos:
+            assert todos.read() == "Pass todo directly.\n"
