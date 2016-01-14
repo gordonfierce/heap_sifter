d2058536edd3218a1e2d400f58491cbee71c51e6
Gordon Fierce <gordon.fierce@gmail.com>
2016-01-13 22:27:00 -0500

    Start testing sift_one.

--- test_heapsifter.py
+++ test_heapsifter.py
@@ -1,4 +1,3 @@
-import click
 from click.testing import CliRunner
 from hypothesis import given, example
 from hypothesis.strategies import text
@@ -20,12 +19,12 @@ def test_add_todo():
         result = runner.invoke(heapsifter.add, input="Write a test.")
         assert result.exit_code == 0
 
-@given(text())
-def test_add_todo(text_arg):
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        result = runner.invoke(heapsifter.add, input=text_arg)
-        assert result.exit_code == 0 
+# @given(text())
+# def test_add_todo(text_arg):
+#     runner = CliRunner()
+#     with runner.isolated_filesystem():
+#         result = runner.invoke(heapsifter.add, input=text_arg)
+#         assert result.exit_code == 0 
 
 def test_add_arg():
     runner = CliRunner()
@@ -44,10 +43,25 @@ def test_is_heap():
     assert is_heap(sorted_heap)
 
 
+def test_sift_one():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open("first.txt", 'w') as first:
+            first.write("Call thing x.\n")
+            first.write("File reciepts.\n")
+            first.write("Remove thumbtacks.\n")
+        with open("second.txt", 'w') as second:
+            second.write("Add stuff.\n")
+            second.write("Break things.\n")
+            second.write("Delete other thing.\n")
+        heapsifter.TODO = dummy_todo
+        runner.invoke(heapsifter.sift_one, ["--source", "second.txt", "--target", "first.txt"]) 
+        # Actually read the items and check is_heap
+
 class dummy_todo(TODO):
     """This class is easily sorted because it always compares
     in importancy by lexographical order."""
     def __lt__(self, other):
-        return self < other
+        return self.text < other.text
     def __eq__(self, other):
-        return self == other
+        return self.text == other.text
